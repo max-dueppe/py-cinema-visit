05461fb169a5161b66590aefee6c860824cade5a
max-dueppe <max.dueppe@pm.me>
2026-01-29 16:26:01 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(movie: str,
-                 customers: list,
+def cinema_visit(customers: list,
                  hall_number: int,
-                 cleaner: str) -> None:
+                 cleaner: str,
+                 movie: str) -> None:
     customers_ = []
     for customer in customers:
         customers_.append(Customer(customer["name"], customer["food"]))
